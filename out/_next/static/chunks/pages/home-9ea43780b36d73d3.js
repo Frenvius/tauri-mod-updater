6cf6fce341ba2907af13145451044f8708be8ad6
Felipe Ribeiro <frenvius@gmail.com>
2023-10-02 01:19:30 -0300

    chore: remove out folder

--- out/_next/static/chunks/pages/home-9ea43780b36d73d3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[229,255],{8862:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/home",function(){return u(7119)}])}},function(n){n.O(0,[981,119,774,888,179],function(){return n(n.s=8862)}),_N_E=n.O()}]);
